af704ec977f8273bd2964c6b594144fffee97efb
Tav-115 <33943446+Tav-115@users.noreply.github.com>
2021-11-19 01:00:49 -0300

    recursos-5

--- src/Components/Sections/Cards-Recursos.js
+++ src/Components/Sections/Cards-Recursos.js
@@ -10,11 +10,11 @@ const CardsRecursos = () => (
     </div>
     
     <div class="s-bg-white s-pxy-2">
-        <h3>Manual Vacunador</h3>
+        <h4>Manual Vacunador</h4>
         <p class="s-mb-0">Dirección General de Redes y Programas de Salud.</p>
     </div>
     <footer class="center s-bg-grey s-pxy-2 s-radius-br s-radius-bl">
-        
+    <p>Descargar</p>
         <a href="https://drive.google.com/file/d/1EgB1dE35siNobPGETCc3GNv9jvdvfMi_/view?usp=sharing" target="_blank"> 
         <div class="button">Páginas: 134</div>
         </a>
@@ -29,11 +29,11 @@ const CardsRecursos = () => (
     </div>
     
     <div class="s-bg-white s-pxy-2">
-        <h3>Planilla Nomivac</h3>
+        <h4>Planilla Nomivac</h4>
         <p class="s-mb-0">Sistema Integrado de Información Sanitaria Argentino.</p>
     </div>
     <footer class="center s-bg-grey s-pxy-2 s-radius-br s-radius-bl">
-        
+    <p>Descargar</p>
         <a href="https://drive.google.com/file/d/1gHMhphAGGBZ39koA51BIqYYlOAFPdBWk/view?usp=sharing" target="_blank"> 
         <div class="button">Páginas: 1</div>
         </a>
@@ -48,11 +48,11 @@ const CardsRecursos = () => (
     </div>
     
     <div class="s-bg-white s-pxy-2">
-        <h3>Actualizacion 2021 - Lineamientos Técnicos</h3>
+        <h4>Actualizacion 2021 - Lineamientos Técnicos</h4>
         <p class="s-mb-0">Octubre 2021.</p>
     </div>
     <footer class="center s-bg-grey s-pxy-2 s-radius-br s-radius-bl ">
-        
+    <p>Descargar</p>
         <a href="https://drive.google.com/file/d/17bVnPnAy4ffgxx8Sh0taeRRiXJsPyu3o/view?usp=sharing" target="_blank"> 
         <div class="button">Páginas: 48</div>
         </a>
@@ -67,16 +67,119 @@ const CardsRecursos = () => (
     </div>
     
     <div class="s-bg-white s-pxy-2">
-        <h3>Dosis Adicional Covid</h3>
+        <h4>Dosis Adicional Covid</h4>
         <p class="s-mb-0">Actualizacion Noviembre 2021.</p>
     </div>
     <footer class="center s-bg-grey s-pxy-2 s-radius-br s-radius-bl ">
-        
+    <p>Descargar</p>
         <a href="https://drive.google.com/file/d/1aBYLCUHsjUR85Cdfsa5WayVb4ZZFEV7r/view?usp=sharing" target="_blank"> 
         <div class="button">Páginas: 14</div>
         </a>
     </footer>
 </article>
+
+<article class="s-shadow-bottom">
+       
+    <div class=" s-ratio-16-9 img-container s-radius-tl s-radius-tr">
+        <img src={`${process.env.PUBLIC_URL}/Assets/images/FormularioFrio.png`} 
+     alt="Planilla Nomivac"/>
+    </div>
+    
+    <div class="s-bg-white s-pxy-2">
+        <h4>Formulario Desvio en Cadena de Frio</h4>
+        <p class="s-mb-0"></p>
+    </div>
+    <footer class=" center s-bg-grey s-pxy-2 s-radius-br s-radius-bl ">
+        
+    <p>Descargar</p>
+        <a href="https://docs.google.com/document/d/1FonhaCJbrqp3qgX27Q6jYng5FBBb63Gn/edit?usp=sharing&ouid=116373942796959660147&rtpof=true&sd=true" target="_blank"> 
+        <div class="button">Páginas: 2</div>
+        </a>
+    </footer>
+</article>
+
+<article class="s-shadow-bottom">
+       
+    <div class="s-ratio-16-9 img-container s-radius-tl s-radius-tr">
+        <img src={`${process.env.PUBLIC_URL}/Assets/images/PlanillaPedidos.png`} 
+     alt="Planilla Nomivac"/>
+    </div>
+    
+    <div class="s-bg-white s-pxy-2">
+        <h4>Planilla de Pedido de Insumos</h4>
+        <p class="s-mb-0"></p>
+    </div>
+    <footer class="center s-bg-grey s-pxy-2 s-radius-br s-radius-bl ">
+        
+    <p>Descargar</p>
+        <a href="https://docs.google.com/document/d/166EjZuHDF8C4QxkUV6upLq6WUPhESpyT/edit?usp=sharing&ouid=116373942796959660147&rtpof=true&sd=true" target="_blank"> 
+        <div class="button">Páginas: 3</div>
+        </a>
+    </footer>
+</article>
+
+<article class="s-shadow-bottom">
+       
+    <div class="s-ratio-16-9 img-container s-radius-tl s-radius-tr">
+        <img src={`${process.env.PUBLIC_URL}/Assets/images/InstructivoEsavi.png`} 
+     alt="Planilla Nomivac"/>
+    </div>
+    
+    <div class="s-bg-white s-pxy-2">
+        <h4>Instructivo ESAVI</h4>
+        <p class="s-mb-0"></p>
+    </div>
+    <footer class="center s-bg-grey s-pxy-2 s-radius-br s-radius-bl ">
+        
+    <p>Descargar</p>
+        <a href="https://drive.google.com/file/d/1XbX5GVAWas2nOwUOjr8ckXEOCds01YHk/view?usp=sharing" target="_blank"> 
+        <div class="button">Páginas: 5</div>
+        </a>
+    </footer>
+</article>
+
+<article class="s-shadow-bottom">
+       
+    <div class="s-ratio-16-9 img-container s-radius-tl s-radius-tr">
+        <img src={`${process.env.PUBLIC_URL}/Assets/images/LeyVacunas.png`} 
+     alt="Planilla Nomivac"/>
+    </div>
+    
+    <div class="s-bg-white s-pxy-2">
+        <h4>Ley 27.491</h4>
+        <p class="s-mb-0"></p>
+    </div>
+    <footer class="b0 center s-bg-grey s-pxy-2 s-radius-br s-radius-bl ">
+        
+    <p>Descargar</p>
+        <a href="https://drive.google.com/file/d/1XbX5GVAWas2nOwUOjr8ckXEOCds01YHk/view?usp=sharing" target="_blank"> 
+        <div class="button">Páginas: 4</div>
+        </a>
+    </footer>
+</article>
+
+
+    <article class="s-shadow-bottom">
+       
+    <div class="s-ratio-16-9 img-container s-radius-tl s-radius-tr">
+        <img src={`${process.env.PUBLIC_URL}/Assets/images/Neumococo.png`} 
+     alt="Planilla Nomivac"/>
+    </div>
+    
+    <div class="s-bg-white s-pxy-2">
+        <h4>Lineamientos Técnicos Neumococo</h4>
+        <p class="s-mb-0"></p>
+    </div>
+    <footer class="center s-bg-grey s-pxy-2 s-radius-br s-radius-bl ">
+        
+    <p>Descargar</p>
+        <a href="https://drive.google.com/file/d/1rUawcsr6vsaDcH67iNsjtuE2EJBEyYNv/view?usp=sharing" target="_blank"> 
+        <div class="button">Páginas: 25</div>
+        </a>
+    </footer>
+</article>
+
+
 </div>
 )
 export default CardsRecursos
