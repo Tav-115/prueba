58890cc8ee1ccde2e2d46d22a821abf4b353da30
Tav-115 <33943446+Tav-115@users.noreply.github.com>
2022-05-26 02:51:52 -0300

    26-05-22

--- src/Components/Sections/slider.js
+++ src/Components/Sections/slider.js
@@ -18,14 +18,18 @@ const Slider = () => (
        link="https://docs.google.com/presentation/d/1yLiDJJxUJbS0RaJOXzuFOJN0ptHDflAl/edit?usp=sharing&ouid=116373942796959660147&rtpof=true&sd=true"/>
 </div>
             <div className="slider--section">
-            
+            <RecursosCard nombre="Moderna" 
+   subt="17-05-2022"
+   image="Ext.Mod.jpg" 
+   pages="2"
+   link="https://drive.google.com/file/d/1SZ1T9DvLo-1b8cyWrDf14hJWGAwxhCGU/view?usp=sharing"/>
             </div>
             <div className="slider--section">
-            <RecursosCard nombre="Módulo 3" 
-       subt="Tablas dinámicas DICEI."
-       image="M3c.jpg" 
-       pages="11"
-       link="https://drive.google.com/file/d/1ZEn35R3D7WIRQ3rGdbGl8AOGPiZIbazy/view?usp=sharing"/>
+            <RecursosCard nombre="Sputnik (I y II)" 
+   subt="03-03-2022"
+   image="Ext.Sput.jpg" 
+   pages="3"
+   link="https://drive.google.com/file/d/1ZMeylmFmqpcMwQuH33hO5YO0NRzJWd7-/view?usp=sharing"/>
 
             </div>
          
